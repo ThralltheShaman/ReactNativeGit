0bfb6e94730a6f42d0956a2a77d9b5a8236ff8d5
Nikita <ihatebreathinginthewild@gmail.com>
2022-02-10 18:26:45 +0500

    added UI for homescreen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,42 @@ import { auth } from '../firebase'
 export default LoginScreen
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+  
+    const navigation = useNavigation()
+  
+    useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged(user => {
+        if (user) {
+          navigation.replace("Home")
+        }
+      })
+  
+      return unsubscribe
+    }, [])
+  
+    const handleSignUp = () => {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredentials => {
+          const user = userCredentials.user;
+          console.log('Registered with:', user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+  
+    const handleLogin = () => {
+      auth
+        .signInWithEmailAndPassword(email, password)
+        .then(userCredentials => {
+          const user = userCredentials.user;
+          console.log('Logged in with:', user.email);
+        })
+        .catch(error => alert(error.message))
+    }
+
   return (
     <KeyboardAvoidingView
     style={styles.container}
