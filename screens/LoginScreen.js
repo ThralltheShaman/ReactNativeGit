e17178bef4f9f160a5fea8ca3b47de05931bf8cf
Nikita <ihatebreathinginthewild@gmail.com>
2022-02-10 18:22:07 +0500

    added styles to container inputs and buttons

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,9 +3,41 @@ import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <KeyboardAvoidingView
+    style={styles.container}
+    behavior="padding"
+  >
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        style={styles.input}
+        secureTextEntry
+      />
     </View>
+
+    <View style={styles.buttonContainer}>
+      <TouchableOpacity
+        onPress={handleLogin}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleSignUp}
+        style={[styles.button, styles.buttonOutline]}
+      >
+        <Text style={styles.buttonOutlineText}>Register</Text>
+      </TouchableOpacity>
+    </View>
+  </KeyboardAvoidingView>
   )
 }
 
